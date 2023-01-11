d8aff80ab34f2100c3ad02294b84e962ed5f9c73
nangeee <miniwoninuna@gmail.com>
2023-01-11 18:50:12 +0900

    Set/Extend base template

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def helloWorld(request):
-    return render(request, template_name="base.html")
+    return render(request, template_name="accountapp/helloWorld.html")
 
     # return HttpResponse("Hello, World!") # Alt + Enter로 라이브러리 import 가능
 
