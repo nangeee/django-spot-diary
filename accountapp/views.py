c0f0c6705318647d0fcfa801c1a7cc249ca4f1f4
nangeee <miniwoninuna@gmail.com>
2023-01-27 23:02:38 +0900

    Set csrf token and rendering function for post method

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,13 @@ from django.shortcuts import render
 
 
 def helloWorld(request):
-    return render(request, template_name="accountapp/helloWorld.html")
+    if request.method == "POST":
+        return render(request, template_name="accountapp/helloWorld.html",
+                      context={"text": "POST METHOD"})
+    else:
+        return render(request, template_name="accountapp/helloWorld.html",
+                      context={"text": "NOT POST METHOD"})
 
+    # return render(request, template_name="accountapp/helloWorld.html")
     # return HttpResponse("Hello, World!") # Alt + Enter로 라이브러리 import 가능
 
