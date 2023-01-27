10e2c125cd857c4a68468bf7643cfcd217c9919a
nangeee <miniwoninuna@gmail.com>
2023-01-27 21:34:05 +0900

    Create model and Make migration - 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userName = models.CharField(max_length=20, null=False)
+    userID = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=25, null=False)
+
+
