d5c435701ded77a0a33834fdd0362621924adef9
nangeee <miniwoninuna@gmail.com>
2023-02-28 01:00:48 +0900

    Override get_success_url method in Profileapp views to send user's pk parameter through url

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -16,7 +16,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "current_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("accountapp:helloWorld")
     template_name = "profileapp/createProfile.html"
 
     def form_valid(self, form):  # profileapp/forms.py의 ProfileCreationForm에서 전송한 데이터가 form 파라미터에 전달됨
@@ -25,6 +24,11 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)  # 이 line만 작성하면, 원래의 form_valid 함수와 동일
 
+    def get_success_url(self):
+        # self.object는 profile
+        # 해당 profile의 user.pk
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, "get")
 @method_decorator(profile_ownership_required, "post")
@@ -32,8 +36,12 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = "current_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("accountapp:helloWorld")
     template_name = "profileapp/update.html"
+
+    def get_success_url(self):
+        # self.object는 profile
+        # 해당 profile의 user.pk
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
 
 
 
